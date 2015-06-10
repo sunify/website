90d4b377a5b9d0d953ef04d79284d7f178958864
Александр Лунев <isuntc@gmail.com>
2015-06-11 02:10:46 +0800

    Some presets

--- app.js
+++ app.js
@@ -1,48 +1,46 @@
 import 'babel/polyfill';
-import { run } from './core';
+import { run, addDot, clear } from './core';
 import dot from './dot';
-import ice from './drawers/ice';
+import preset from './presets/neuro';
 
 const { sin, cos, round, random, min, PI } = Math;
 
 let canvas = document.getElementById('cnv');
 let ctx = canvas.getContext('2d');
 
-let dots = [];
+run(ctx, preset);
 
-// for(let i = 0; i < 15; i ++) {
-// 	for(let j = 0; j < 8; j ++) {
-// 		dots.push(dot(i * 140 * (random() * 0.3 + 0.3), j * 200 * (random() * 0.3 + 0.3)));
-// 	}
-// }
 
-run(dots, ctx, ice);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 window.new_dots = [];
-
-let allowDraw = false;
-document.addEventListener('mousedown', (evt) => {
-	allowDraw = true;
-});
-document.addEventListener('mouseup', (evt) => {
-	allowDraw = false;
-});
 document.addEventListener('mousemove', (evt) => {
-	if(allowDraw) {
+	if(evt.which === 1) {
 		const d = dot(
 			evt.pageX / window.innerWidth * 1000,
 			evt.pageY / window.innerHeight * 700
 		);
-		dots.push(d);
+		addDot(d);
 		window.new_dots.push(d);
 	}
 });
 
-document.getElementById('clear').addEventListener('click', () => {
-	dots = [];
-	ctx.fillStyle = '#1E0E33';
-	ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-});
+document.getElementById('clear').addEventListener('click', clear);
 
 window.addEventListener('resize', () => {
 	canvas.width = window.innerWidth + 2;
--- core.js
+++ core.js
@@ -3,44 +3,54 @@ import raf from 'raf';
 
 const { sin, cos, round, random, min, PI } = Math;
 
+let _preset;
+let _ctx;
+let _dots;
+
 let voronoi = new Voronoi();
-let options = {};
 let diagram = null;
 
-const currentPos = (time, dot) => {
-	const speed = time / 200 / dot.speed * PI;
-	const x1 = cos(speed) * 5;
-	const y1 = sin(speed) * dot.ex * dot.reverse * 5;
+function currentPos(time, dot) {
+	const speed = time / (_preset.time || 400) / dot.speed * PI;
+	const x1 = cos(speed) * (_preset.offset || 5);
+	const y1 = sin(speed) * dot.ex * dot.reverse * (_preset.offset || 5);
 
 	dot.x = dot._x / 1000 * window.innerWidth + (x1 * cos(dot.angle) - y1 * sin(dot.angle));
 	dot.y = dot._y / 700 * window.innerHeight + (x1 * sin(dot.angle) + y1 * cos(dot.angle));
 }
-// 
+
 function update(dots, ctx, time) {
 	voronoi.recycle(diagram);
 
-	for(let i = 0, max = options.dots.length; i < max; i += 1) {
-		currentPos(time, options.dots[i]);
+	for(let i = 0, max = _dots.length; i < max; i += 1) {
+		currentPos(time, _dots[i]);
 	}
 
 	diagram = voronoi.compute(
-		options.dots,
-		{xl: 0, xr: options.ctx.canvas.width, yt: 0, yb: options.ctx.canvas.height}
+		_dots,
+		{xl: 0, xr: _ctx.canvas.width, yt: 0, yb: _ctx.canvas.height}
 	);
 
-	options.draw.apply(options.ctx, [diagram]);
+	_preset.drawer.apply(_ctx, [diagram]);
 }
 
 export function addDot(dot) {
-	options.dots.push(dot);
+	_dots.push(dot);
 }
 
-export function run(dots, ctx, draw) {
-	options = { dots, ctx, draw };
+export function clear() {
+	_dots = [];
+	_ctx.canvas.width = _ctx.canvas.width;
+}
+
+export function run(ctx, preset) {
+	_ctx = ctx;
+	_dots = preset.dots.slice();
+	_preset = preset;
 
 	function loop(time) {
 		setTimeout(() => raf(loop), 1000/20);
-		update(dots, ctx, time);
+		update(_dots, _ctx, time);
 	}
 
 	raf(loop);
