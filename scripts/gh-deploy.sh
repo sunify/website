51705fa92b30169bcd04ffb1e82bc792d54e746a
Alex Lunyov <isuntc@gmail.com>
2019-02-04 15:19:30 +0800

    Test deploy with travis

---
+++ scripts/gh-deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
+set -o errexit
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then exit 0; fi
+
+# config
+git config --global user.email "isuntc@gmail.com"
+git config --global user.name "Alex Lunyov"
+
+cd dist
+git init
+git add .
+git commit -m "Deploy to Github Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/sunify/sunify.github.io.git" master:master > /dev/null 2>&1
